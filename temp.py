a796eea24f6b26301bb5d7563a8511672133d42f
borjamir <borja.mir93@gmail.com>
2015-09-24 17:07:13 -0300

    pregunta2

--- temp.py
+++ temp.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.integrate as sinteg
 
 plt.figure(1)
 plt.clf()
@@ -17,3 +18,19 @@ plt.xlabel('x log10[Longitud de Onda (Angstrom)]')
 plt.ylabel('y log10[Flujo (CGS)]')
 plt.title('Espectro del Sol (Flujo vs Longitud de Onda)')
 plt.legend()
+
+"""
+Dado que x1-x0 = 2 = constante podemos considerarlo como un trazador lineal
+por lo que podemos aplicar el metodo del trapecio
+"""
+
+contador=0
+integral=0
+trapecio=0
+while contador<1696:
+    trapecio=flujo[contador]+flujo[contador+1] #no multiplico porque delta x=2
+    integral=integral+trapecio
+    contador=contador+1
+print integral
+
+integralcalc=sinteg.trapz(flujo,longitud)
