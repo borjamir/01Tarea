3b27ba8489fb37f72249e6728dafdd03f82dbe93
borjamir <borja.mir93@gmail.com>
2015-09-24 21:23:59 -0300

    pregunta2 definitiva

--- temp.py
+++ temp.py
@@ -10,7 +10,7 @@ plt.clf()
 datos=np.genfromtxt('sun_AM0.dat',float)
 
 longitud=datos[:,0]*10 #Angstroms
-flujo=datos[:,1]*10**10
+flujo=datos[:,1]*10**11
 
 plt.plot(np.log10(longitud), np.log10(flujo))   
 
@@ -20,7 +20,7 @@ plt.title('Espectro del Sol (Flujo vs Longitud de Onda)')
 plt.legend()
 
 """
-Dado que x1-x0 = 2 = constante podemos considerarlo como un trazador lineal
+Dado que x1-x0 = 20 = constante podemos considerarlo como un trazador lineal
 por lo que podemos aplicar el metodo del trapecio
 """
 
@@ -28,9 +28,14 @@ contador=0
 integral=0
 trapecio=0
 while contador<1696:
-    trapecio=flujo[contador]+flujo[contador+1] #no multiplico porque delta x=2
+    trapecio=(flujo[contador]+flujo[contador+1])*10 #dividiendo la altura (20) por 2
     integral=integral+trapecio
     contador=contador+1
 print integral
 
 integralcalc=sinteg.trapz(flujo,longitud)
+
+#superficie esférica a la distancia en que se encuentra la Tierra 2.81e23 m2
+ctesolar=integral*10**-11 # en W/m2
+
+luminosidadtot= ctesolar*2.81e23
